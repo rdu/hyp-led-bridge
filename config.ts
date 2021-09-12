626cb2285c203464890402356a53fc0bf770f5ef
Booboo <bbootgstore@gmail.com>
2021-09-12 14:18:23 +0200

    K

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 export const CONFIG = {
     MQTT_SERVER: 'mqtt.moerse',
     HYPERION_PORT: 19444,
-    HYPERION_HOST: '10.10.0.124',
+    HYPERION_HOST: '10.69.0.152',
     profiles: {
         0: {
             instance: 0,
